176a3d8fbd45891770cc6cb894f6000576df58bc
ScottEBean <scott.e.bean@gmail.com>
2018-12-18 15:42:31 -0700

    Pretty much a total redesign. Movement timing is an issue to work out as well as timing.

--- scripts/game.js
+++ scripts/game.js
@@ -1,32 +1,127 @@
 //https://bft.usu.edu/kv9rq
 Game.screens['game-play'] = (function (input, graphics, records, menu) {
 
-	var BLOCKSIZE = 25;
-	var CANVASWIDTH = 750;
+	let BLOCKSIZE = 25;
+	let cancelNextRequest = false;
+	let play = false;
+	let didEat = false;
+	let dx = 0;
+	let dy = 0;
+	let grid = new Array(30);
+	let keyboard = input.Keyboard();
+	let scores = records;
+	let score = 0;
+	let speed = 0;
+	let snake = [];
+	let obstacles = [];
+	let apple = { row: 0, col: 0 };
+	let timeAccumultor = 150;
 
-	var cancelNextRequest = false;
+	// 0: empty 
+	// 1: obstacle
+	// 2: snake
+	// 3: apple
 
-	var keyboard = input.Keyboard();
-	var scores = records;
-	var apple = graphics.Apple();
-	var snake = graphics.Snake()
-	var obstacles = graphics.Obstacles();
-	var score = 0;
-	var speed = 0;
-	var didEat = false;
+	function addSegment() {
+		let lastRow = snake[snake.length - 1].row;
+		let lastCol = snake[snake.length - 1].col;
+
+		if (dy > 0 && dx == 0) { snake.push({ row: lastRow + BLOCKSIZE, col: lastCol }); return; }
+		if (dy < 0 && dx == 0) { snake.push({ row: lastRow - BLOCKSIZE, col: lastCol }); return; }
+		if (dx < 0 && dy == 0) { snake.push({ row: lastRow, col: lastCol - BLOCKSIZE }); return; }
+		if (dx > 0 && dy == 0) { snake.push({ row: lastRow, col: lastCol + BLOCKSIZE }); return; }
+	}
+
+	function createApple() {
+		grid[apple.row][apple.col] = 0;
+		let appleCol = Random.nextRange(0, 29);
+		let appleRow = Random.nextRange(0, 29);
+
+		while (grid[appleRow][appleCol] != 0) {
+			appleCol = Random.nextRange(0, 29);
+			appleRow = Random.nextRange(0, 29);
+		}
+
+		grid[appleRow][appleCol] = 3;
+		apple = { row: appleRow, col: appleCol };
+	}
+
+	function blocksIntersect(blockArr, x, y) {
+		if (blockArr.length == 0) { return false; }
+		for (let i = 0; i < blockArr.length; i++) {
+			let r1 = blockArr[i].getCoords();
+			if (intersection(r1.x, r1.x + BLOCKSIZE, r1.y, r1.y + BLOCKSIZE, x, x + BLOCKSIZE, y, y + BLOCKSIZE)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function createObstacles() {
+		let count = 0;
+
+		while (count <= 10) {
+			let obRow = Random.nextRange(0, 29);
+			let obCol = Random.nextRange(0, 29);
+
+			while (grid[obRow][obCol] != 0) {
+				obRow = Random.nextRange(0, 29);
+				obCol = Random.nextRange(0, 29);
+			}
+
+			count++;
+			grid[obRow][obCol] = 1;
+		}
+	}
+
+	function detectCollision() {
+		const head = snake[0];
+
+		//walls and ceiling
+		if (head.row < 0 || head.row > 29 || head.col < 0 || head.col > 29) {
+			cancelNextRequest = true;
+			gameOver();
+		}
+
+		// obstacles
+		for (let i = 0; i < grid.length; i++) {
+			for (let j = 0; j < grid.length; j++) {
+				if (grid[i][j] == 1 && head.row == i && head.col == j) {
+					cancelNextRequest = true;
+					gameOver();
+				}
+			}
+		}
+
+		//apple
+		if (head.row == apple.row && head.col == apple.col) {
+			didEat = true;
+		}
+
+		// snake collides with self			
+		if (SelfCollision(head)) {
+			cancelNextRequest = true;
+			gameOver();
+		}
+	}
 
 	function initialize() {
+		initializeGrid();
+		initializeSnake();
+		createApple();
+		createObstacles();
+		grid[14][16] = 0;
+		grid[14][15] = 0;
+		grid[14][14] = 0;
+		grid[14][13] = 0;
 		score = 0;
 		speed = 0;
 		Game.records.initialize();
-		snake.init();
-		snake.draw();
-		obstacles.draw();
 
-		keyboard.registerCommand(KeyEvent.DOM_VK_LEFT, snake.moveLeft(-speed));
-		keyboard.registerCommand(KeyEvent.DOM_VK_RIGHT, snake.moveRight(speed));
-		keyboard.registerCommand(KeyEvent.DOM_VK_UP, snake.moveUp(-speed));
-		keyboard.registerCommand(KeyEvent.DOM_VK_DOWN, snake.moveDown(speed));
+		keyboard.registerCommand(KeyEvent.DOM_VK_LEFT, moveLeft);
+		keyboard.registerCommand(KeyEvent.DOM_VK_RIGHT, moveRight);
+		keyboard.registerCommand(KeyEvent.DOM_VK_UP, moveUp);
+		keyboard.registerCommand(KeyEvent.DOM_VK_DOWN, moveDown);
 		keyboard.registerCommand(KeyEvent.DOM_VK_SPACE, go);
 		keyboard.registerCommand(KeyEvent.DOM_VK_R, run);
 		keyboard.registerCommand(KeyEvent.DOM_VK_I, initialize)
@@ -36,112 +131,40 @@ Game.screens['game-play'] = (function (input, graphics, records, menu) {
 		});
 	}
 
-	function createApple() {
-		var appleX = Random.nextRange(0, 29) * BLOCKSIZE;
-		var appleY = Random.nextRange(0, 29) * BLOCKSIZE;
-		let verified = !graphics.BlocksIntersect(obstacles.getBlocks(), appleX, appleY) && !graphics.BlocksIntersect(snake.getSegments(), appleX, appleY);
-
-		while (!verified) {
-			appleX = Random.nextRange(0, 29) * BLOCKSIZE;
-			appleY = Random.nextRange(0, 29) * BLOCKSIZE;
-			verified = !graphics.BlocksIntersect(obstacles.getBlocks(), appleX, appleY) && !graphics.BlocksIntersect(snake.getSegments(), appleX, appleY);
+	function initializeGrid() {
+		for (let i = 0; i < grid.length; i++) {
+			grid[i] = new Array(30);
 		}
-
-		apple.set({ x: appleX, y: appleY });
-
-		console.log('appleX: ' + apple.getCoords().x);
-		console.log('appleY: ' + apple.getCoords().y);
+		for (i = 0; i < grid.length; i++) {
+			for (let j = 0; j < grid.length; j++) {
+				grid[i][j] = 0;
+			}
+		}
 	}
 
-	function detectCollision() {
-		let head = snake.getHead();
+	function initializeSnake() {
+		snake.push({ row: 14, col: 16 });
+		snake.push({ row: 14, col: 15 });
+		snake.push({ row: 14, col: 14 });
+		snake.push({ row: 14, col: 13 });
+		grid[14][16] = 2;
+		grid[14][15] = 2;
+		grid[14][14] = 2;
+		grid[14][13] = 2;
+	}
 
-		//walls and ceiling
-		if (head.getCoords().x <= 0 ||
-				head.getCoords().x + BLOCKSIZE >= CANVASWIDTH ||
-				head.getCoords().y <= 0 ||
-				head.getCoords().y >= CANVASWIDTH) {
-			cancelNextRequest = true;
-			//gameOver();
-		}
-
-		//obstacles
-		// if (graphics.BlocksIntersect(obstacles.getBlocks, head.getCoords().x, head.getCoords().y)) {
-		// 	cancelNextRequest = true;
-		// 	gameOver();
-		// }
-
-		//apple
-		if (graphics.intersection(head.x, head.x + BLOCKSIZE, head.y, head.y + BLOCKSIZE, apple.getCoords().x, apple.getCoords().x + BLOCKSIZE, apple.getCoords().y, apple.getCoords().y + BLOCKSIZE)) {
-			didEat = true;
-			score++;
-			createApple();
-		}
-
-		// snake collides with self			
-		if (SelfCollision(head)) {
-			cancelNextRequest = true;
-			//gameOver();
-		}
+	function intersection(r1l, r1r, r1t, r1b, r2l, r2r, r2t, r2b) {
+		return !(r2l > r1r ||
+			r2r < r1l ||
+			r2t > r1b ||
+			r2b < r1t);
 	}
 
 	function SelfCollision(head) {
-		if (snake.getSegments().length < 4) { return false; }
-		for (let i = 3; i < snake.getSegments().length; i++) {
-			let seg = snake.getSegments()[i];
-			if (graphics.intersection(head.x, head.x + BLOCKSIZE, head.y, head.y + BLOCKSIZE, seg.x, seg.x + BLOCKSIZE, seg.y, seg.y + BLOCKSIZE)) {
-				return true;
-			}
+		for (let i = 3; i < snake.length; i++) {
+			if (head.row == snake[i].row && head.col == snake[i].col) { return true; }
 		}
 		return false;
-	}
-
-	function processInput(elapsedTime) {
-		keyboard.update(elapsedTime);
-	}
-
-	function gameOver() {
-		graphics.setLargeTextProperties();
-		graphics.drawText("Game Over :(", 25, 350);
-		setTimeout(function () { menu.showScreen('main-menu'), 1000 });
-		scores.update(score);
-	}
-
-	function go() {
-		if (speed > 0) { return; }
-		speed = 150;
-		snake.draw();
-		apple.draw();
-		//obstacles.draw();
-		graphics.clearTopLayer();
-		graphics.setCountdownTextProperties();
-		graphics.drawText("3", 187, 250);
-		setTimeout(function () { graphics.drawText("2", 375, 200); }, 1000);
-		setTimeout(function () { graphics.drawText("1", 562, 200); }, 2000);
-		setTimeout(function () { graphics.setLargeTextProperties(); }, 2999);
-		setTimeout(function () { graphics.drawText("Go!", 280, 500); }, 3000);
-		setTimeout(function () { graphics.clearTopLayer(); }, 3300);
-		setTimeout(function () { speed = 150; }, 3300);
-	}
-	
-	function run() {
-		lastTimeStamp = performance.now();
-		cancelNextRequest = false;
-		requestAnimationFrame(gameLoop);
-		console.log('Snakes on a Plane!');
-	}
-
-	function update(elapsedTime) {
-		detectCollision();
-		snake.update(didEat);
-	}
-
-	function render() {
-		graphics.clear();
-		snake.draw();
-		obstacles.draw();
-		apple.draw();
-		graphics.drawScore(score);
 	}
 
 	function gameLoop(currentTime) {
@@ -153,7 +176,95 @@ Game.screens['game-play'] = (function (input, graphics, records, menu) {
 		if (!cancelNextRequest) {
 			requestAnimationFrame(gameLoop);
 		}
-	};
+	}
+
+	function gameOver() {
+		graphics.setLargeTextProperties();
+		graphics.drawText("Game Over :(", 25, 350);
+		setTimeout(function () { menu.showScreen('main-menu'), 1000 });
+		scores.update(score);
+	}
+
+	function go() {
+		if (speed > 0) { return; }
+		graphics.clearTopLayer();
+		graphics.setCountdownTextProperties();
+		graphics.drawText("3", 187, 250);
+		setTimeout(function () { graphics.drawText("2", 375, 250); }, 1000);
+		setTimeout(function () { graphics.drawText("1", 562, 250); }, 2000);
+		setTimeout(function () { graphics.setLargeTextProperties(); }, 2999);
+		setTimeout(function () { graphics.drawText("Go!", 280, 500); }, 3000);
+		setTimeout(function () { graphics.clearTopLayer(); }, 3300);
+		setTimeout(function () { speed = 150; go = true; dx = 1;}, 3300);
+	}
+
+	function moveDown() {
+		if (dy < 0) { return; }
+		dx = 0;
+		dy = 1;
+	}
+
+	function moveLeft() {
+		if (dx > 0) { return; }
+		dx = -1;
+		dy = 0;
+	}
+
+	function moveRight() {
+		if (dx < 0) { return; }
+		dx = 1;
+		dy = 0;
+	}
+
+	function moveUp() {
+		if (dy > 0) { return; }
+		dx = 0;
+		dy = -1
+	}
+
+	function processInput(elapsedTime) {
+		keyboard.update(elapsedTime);
+	}
+
+	function render() {
+		graphics.clear();
+
+		for (let i = 0; i < grid.length; i++) {
+			for (let j = 0; j < grid.length; j++) {
+				if (grid[i][j] == 1) { graphics.drawObstacle(i, j); }
+				if (grid[i][j] == 3) { graphics.drawApple(i, j); }
+			}
+		}
+
+		for (let i = 0; i < snake.length; i++) {
+			graphics.drawSnakeSegment(snake[i].row, snake[i].col);
+		}
+
+		graphics.drawScore(score);
+	}
+
+	function run() {
+		lastTimeStamp = performance.now();
+		cancelNextRequest = false;
+		requestAnimationFrame(gameLoop);
+		console.log('Snakes on a Plane!');
+	}
+
+	function update(elapsedTime) {
+			timeAccumultor -= elapsedTime;
+			if (timeAccumultor > 0) { return; }
+			timeAccumultor = 5000;
+			detectCollision();
+			const head = { row: snake[0].row + dy * BLOCKSIZE, col: snake[0].row + dx * BLOCKSIZE };
+			snake.unshift(head);
+
+			if (didEat) {
+				didEat = false;
+				score++;
+				createApple();
+			}
+			else { snake.pop(); }
+	}
 
 	return {
 		initialize: initialize,
--- scripts/graphics.js
+++ scripts/graphics.js
@@ -1,5 +1,5 @@
 // ------------------------------------------------------------------
-// This is the graphics object.  The various rendering components
+// This is the graphics object.  The letious rendering components
 // are located here.
 // ------------------------------------------------------------------
 Game.graphics = (function () {
@@ -10,9 +10,8 @@ Game.graphics = (function () {
 	let gameContext = gameCanvas.getContext('2d');
 	let topLayerCanvas = document.getElementById('topLayerCanvas');
 	let topLayerContext = topLayerCanvas.getContext('2d');
-	let bodyFillStyle = 'rgb(0, 208, 208)';  //The best cyan
-	let headFillStyle = 'rgba(0, 255, 0, 1)'; // Very Green
-	let blockFillStyle = 'rgba(225, 225, 0, 1)'; // Very Yellow
+	let snakeFillStyle = 'rgba(0, 255, 0, 1)'; // Very Green
+	let obstacleFillStyle = 'rgba(0, 208, 208, 1)'; // Cyan
 	let appleFillStyle = 'rgba(255, 0, 0, 1)'; //Very Red
 	let BLOCKSIZE = 25;
 
@@ -33,29 +32,36 @@ Game.graphics = (function () {
 		topLayerContext.restore();
 	}
 
+	function drawApple(row, col) {
+		drawBlock({ x: col * BLOCKSIZE, y: row * BLOCKSIZE, fillStyle: appleFillStyle });
+	}	
+
 	function drawBackground() {
-		backgroundContext.fillStyle = "rgba(0, 0, 0, 0.25)";
-		backgroundContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-		backgroundContext.fillStyle = 'red';
+
 	}
 
-	function setCountdownTextProperties() {
-		topLayerContext.font = '45px Roboto';
-		topLayerContext.fillStyle = "rgba(0, 0, 0, 0.25)";
-		topLayerContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-		topLayerContext.fillStyle = '#00d0d0';
+	function drawBlock(spec) {
+		gameContext.save();
+		gameContext.fillStyle = spec.fillStyle;
+		gameContext.strokestyle = 'black';
+		gameContext.fillRect(spec.x, spec.y, BLOCKSIZE, BLOCKSIZE);
+		gameContext.strokeRect(spec.x, spec.y, BLOCKSIZE, BLOCKSIZE);
+		gameContext.restore();
 	}
 
-	function setScoreTextProps() {
-		gameContext.font = '15px Roboto';
-		gameContext.fillStyle = '#00d0d0';
+	function drawObstacle(row, col) {
+		drawBlock({ x: col * BLOCKSIZE, y: row * BLOCKSIZE, fillStyle: obstacleFillStyle });
 	}
 
-	function setLargeTextProperties() {
-		topLayerContext.font = '125px Roboto';
-		topLayerContext.fillStyle = "rgba(0, 0, 0, 0.25)";
-		topLayerContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-		topLayerContext.fillStyle = '#00d0d0';
+	function drawScore(score) {
+		setScoreTextProps();
+		let scoreText = 'Score: ' + score;
+		let textWidth = gameContext.measureText(scoreText).width + 10;
+		gameContext.fillText(scoreText, gameCanvas.width - textWidth, gameCanvas.height - 10);
+	}	
+
+	function drawSnakeSegment(row, col) {
+		drawBlock({ x: col * BLOCKSIZE, y: row * BLOCKSIZE, fillStyle: snakeFillStyle });
 	}
 
 	function drawText(text, x, y) {
@@ -67,220 +73,37 @@ Game.graphics = (function () {
 		//console.log(topContext.measureText(text));
 	}
 
-	function drawScore(score) {
-		setScoreTextProps();
-		var scoreText = 'Score: ' + score;
-		var textWidth = gameContext.measureText(scoreText).width + 10;
-		gameContext.fillText(scoreText, gameCanvas.width - textWidth, gameCanvas.height - 10);
+	function setCountdownTextProperties() {
+		topLayerContext.font = '45px Roboto';
+		topLayerContext.fillStyle = "rgba(0, 0, 0, 0.25)";
+		topLayerContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+		topLayerContext.fillStyle = '#00d0d0';
 	}
 
-	function intersection(r1l, r1r, r1t, r1b, r2l, r2r, r2t, r2b) {
-		return !(r2l > r1r ||
-			r2r < r1l ||
-			r2t > r1b ||
-			r2b < r1t);
+	function setLargeTextProperties() {
+		topLayerContext.font = '125px Roboto';
+		topLayerContext.fillStyle = "rgba(0, 0, 0, 0.25)";
+		topLayerContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+		topLayerContext.fillStyle = '#00d0d0';
 	}
 
-	function BlocksIntersect(blockArr, x, y) {
-		if (blockArr.length == 0) { return false; }
-		for (let i = 0; i < blockArr.length; i++) {
-			var r1 = blockArr[i].getCoords();
-			if (intersection(r1.x, r1.x + BLOCKSIZE, r1.y, r1.y + BLOCKSIZE, x, x + BLOCKSIZE, y, y + BLOCKSIZE)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	function Apple() {
-		that = {};
-		var coords = {
-			x: 0,
-			y: 0
-		};
-
-		that.set = function (spec) {
-			coords.x = spec.x;
-			coords.y = spec.y;
-		}
-
-		that.getCoords = function () {
-			return coords;
-		}
-
-		that.draw = function () {
-			gameContext.save();
-			gameContext.fillStyle = appleFillStyle;
-			gameContext.strokestyle = 'black';
-			gameContext.fillRect(coords.x, coords.y, BLOCKSIZE, BLOCKSIZE);
-			gameContext.strokeRect(coords.x, coords.y, BLOCKSIZE, BLOCKSIZE);
-			gameContext.restore();
-		}
-
-		return that;
-	}
-
-	function Obstacles() {
-		that = {};
-		var blocks = [];
-		var count = 0;
-
-		while (count <= 15) {
-			let testX = Random.nextRange(0, 29) * BLOCKSIZE;
-			let testY = Random.nextRange(0, 29) * BLOCKSIZE;
-			if (!BlocksIntersect(blocks, testX, testY)) {
-				blocks.push(new Obstacle({ x: testX, y: testY }));
-				count++;
-			}
-		}
-
-		that.getBlocks = function () {
-			return blocks;
-		}
-
-		that.draw = function () {
-			for (let i = 0; i < blocks.length; i++) {
-				blocks[i].draw();
-			}
-		}
-
-		return that;
-	};
-
-	function Obstacle(spec) {
-		that = {};
-		var coords = {
-			x: spec.x,
-			y: spec.y
-		};
-
-		that.getCoords = function () {
-			return coords;
-		}
-
-		that.draw = function () {
-			gameContext.save();
-			gameContext.fillStyle = blockFillStyle;
-			gameContext.strokestyle = 'black';
-			gameContext.fillRect(coords.x, coords.y, BLOCKSIZE, BLOCKSIZE);
-			gameContext.strokeRect(coords.x, coords.y, BLOCKSIZE, BLOCKSIZE);
-			gameContext.restore();
-		}
-
-		return that;
-	}
-
-	function Snake(spec) {
-		that = {};
-		var dirX = 0;
-		var dirY = 0;
-		var segments = [];
-		var didEat = false;
-
-		that.init = function () {
-			segments.push(new Segment({
-				x: Random.nextRange(0, 29) * 10,
-				y: Random.nextRange(0, 29) * 10
-			}));
-			console.log(segments[0]);
-			console.log('headx: ' + segments[0].getCoords().x);
-			console.log('heady: ' + segments[0].getCoords().y);
-		}
-
-		that.getHead = function () {
-			return segments[0];
-		}
-
-		that.getSegments = function () {
-			return segments;
-		}
-
-		that.draw = function () {
-			for (let i = 0; i < segments.length; i++) {
-				segments[i].draw();
-			}
-		}
-
-		that.update = function () {
-			segments[0].update({x: dirX, y: dirY});
-			const head = segments[0];
-			segments.unshift(head);
-
-			if (didEat) {
-				didEat = false;
-			} else {
-				segments.pop();
-			}
-		}
-
-		that.moveUp = function (dy) {
-			if (dy < 0) { return; }
-			dirY = dy;
-			dirX = 0;
-		}
-
-		that.moveDown = function (dy) {
-			if (dy > 0) { return; }
-			dirY = dy;
-			dirX = 0;
-		}
-
-		that.moveLeft = function (dx) {
-			if (dx > 0) { return; }
-			dirX = dx;
-			dirY = 0;
-		}
-
-		that.moveRight = function (dx) {
-			if (dx > 0) { return; }
-			dirX = dx;
-			dirY = 0;
-		}	
-
-		return that;
-	}
-
-	function Segment(spec) {
-		that = {};
-		var coords = {
-			x: spec.x,
-			y: spec.y
-		};
-
-		that.getCoords = function () {
-			return coords;
-		}
-
-		that.update = function(spec){
-			coords.x = spec.x;
-			coords.y = spec.y;
-		}
-
-		that.draw = function () {
-			gameContext.save();
-			gameContext.fillStyle = bodyFillStyle;
-			gameContext.strokestyle = 'black';
-			gameContext.fillRect(coords.x, coords.y, BLOCKSIZE, BLOCKSIZE);
-			gameContext.strokeRect(coords.x, coords.y, BLOCKSIZE, BLOCKSIZE);
-			gameContext.restore();
-		}
-
-		return that;
-	}
+	function setScoreTextProps() {
+		gameContext.font = '15px Roboto';
+		gameContext.fillStyle = '#00d0d0';
+	}	
 
 	return {
-		drawBackground: drawBackground,
-		drawScore: drawScore,
-		setCountdownTextProperties: setCountdownTextProperties,
-		setLargeTextProperties: setLargeTextProperties,
-		drawText: drawText,
-		clearTopLayer: clearTopLayer,
 		clear: clear,
-		BlocksIntersect: BlocksIntersect,
-		intersection: intersection,
-		Snake: Snake,
-		Obstacles: Obstacles,
-		Apple: Apple
+		clearTopLayer: clearTopLayer,
+		drawApple: drawApple,
+		drawBackground: drawBackground,		
+		drawObstacle: drawObstacle,
+		drawScore: drawScore,
+		drawSnakeSegment: drawSnakeSegment,	
+		drawText: drawText,	
+		setCountdownTextProperties: setCountdownTextProperties,
+		setLargeTextProperties: setLargeTextProperties,		
+		setScoreTextProps: setScoreTextProps
 	};
 
 }());
